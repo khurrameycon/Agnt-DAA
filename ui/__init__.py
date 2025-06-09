94743dcdaa389f95437a91b93604839f4a43ccc7
Khurram <khurrameycon@gmail.com>
2025-06-09 18:47:00 +0500

    Delete ui directory - wrong copied

--- ui/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Core components for sagax1
-"""
--- ui/dialogs/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Dialog components for sagax1
-"""
-
-from app.ui.dialogs.create_agent_dialog import CreateAgentDialog
-
-__all__ = ["CreateAgentDialog"]
